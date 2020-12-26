a10c4cc0b84ec1efa61d239099eccdec3afa2cb1
Andrew Bullock <trullock@gmail.com>
2020-12-26 16:44:11 +0000

    added dollar

---
+++ dollar/src/index.js
@@ -0,0 +1,26 @@
+HTMLElement.prototype.addClickListener = function(cb, useCapture){
+	return this.addEventListener('click', e => {
+		e.preventDefault()
+		cb.call(this, e);
+	}, useCapture || false);
+}
+HTMLElement.prototype.$$ = function (selector) {
+	return this.querySelectorAll(selector);
+}
+HTMLElement.prototype.$ = function (selector) {
+	return this.querySelector(selector);
+}
+HTMLElement.prototype.remove = function () {
+	return this.parentElement.removeChild(this);
+}
+HTMLDocument.prototype.$$ = function (selector) {
+	return this.querySelectorAll(selector);
+}
+HTMLDocument.prototype.$ = function (selector) {
+	return this.querySelector(selector);
+}
+
+HTMLElement.prototype.empty = function () {
+	while(this.firstElementChild)
+		this.removeChild(this.firstElementChild);
+}
