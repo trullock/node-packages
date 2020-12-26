2a7631dd9b050688429c79b5db233ccf9600b169
Andrew Bullock <trullock@gmail.com>
2020-12-26 16:39:31 +0000

    added pubsub

---
+++ pubsub/src/index.js
@@ -0,0 +1,128 @@
+
+/*
+A function to represent a queue
+Created by Stephen Morley - http://code.stephenmorley.org/ - and released under the terms of the CC0 1.0 Universal legal code: http://creativecommons.org/publicdomain/zero/1.0/legalcode
+*/
+var Queue = function () {
+	var queue = [];
+	var offset = 0;
+	var me = {};
+
+	function getLength() {
+		return (queue.length - offset);
+	}
+
+	me.isEmpty = function () {
+		return (queue.length === 0);
+	};
+
+	me.enqueue = function (item) {
+		queue.push(item);
+	};
+
+	me.dequeue = function () {
+		if (queue.length === 0)
+			return undefined;
+
+		var item = queue[offset];
+
+		if (++offset * 2 >= queue.length) {
+			queue = queue.slice(offset);
+			offset = 0;
+		}
+
+		return item;
+	};
+
+	me.peek = function () {
+		return (queue.length > 0 ? queue[offset] : undefined);
+	};
+
+	return me;
+};
+
+
+var me = {};
+var handlers = {};
+var queue = new Queue();
+var processing;
+
+function processQueue() {
+	while (!queue.isEmpty()) {
+		var action = queue.dequeue();
+		action.call(null);
+	}
+}
+
+function enqueue(handler, args) {
+	queue.enqueue(function () {
+		handler.apply(null, args);
+	});
+}
+
+me.subscribe = function (type, func) {
+	if (!handlers[type])
+		handlers[type] = [];
+
+	for (var i = 0; i < handlers[type].length; i++)
+		if (handlers[type][i] === func)
+			throw "Handler already subscribed to this message";
+
+	handlers[type].push(func);
+
+	return this;
+};
+
+me.subscribeOnce = function (type, func) {
+	var handler = function () {
+		me.unsubscribe(handler);
+
+		func.apply(null, arguments);
+	};
+	me.subscribe(type, handler);
+};
+
+me.publish = function (type) {
+	if (!handlers[type])
+		return;
+
+	for (var i in handlers[type]) {
+		var args = [];
+		for (var j = 1; j < arguments.length; j++) {
+			args.push(arguments[j]);
+		}
+
+		enqueue(handlers[type][i], args);
+	}
+
+	if (!processing) {
+		while (!queue.isEmpty()) {
+			processing = true;
+			processQueue();
+		}
+		processing = false;
+	}
+	return this;
+};
+
+me.unsubscribeAll = function (type) {
+	if (type)
+		delete handlers[type]
+	else
+		handlers = {};
+};
+
+me.unsubscribe = function (handler) {
+	for (var type in handlers) {
+		for (var i in handlers[type]) {
+			if (handlers[type][i] === handler) {
+				handlers[type].splice(i, 1);
+				if (handlers[type].length === 0)
+					delete handlers[type];
+				return;
+			}
+		}
+	}
+};
+
+export default me;
