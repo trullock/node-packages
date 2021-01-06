465d4f14c6eee33357cb28e47a49478158f95051
Andrew Bullock <trullock@gmail.com>
2021-01-06 13:25:52 +0000

    added message parameter to pageshowerror

--- page-manager/src/pageshowerror.js
+++ page-manager/src/pageshowerror.js
@@ -1,6 +1,6 @@
 export class PageShowError extends Error {
-	constructor(url, data, action) {
-		super('Error showing requested page')
+	constructor(url, message, data, action) {
+		super(message || 'Error showing requested page')
 
 		// Maintains proper stack trace for where our error was thrown (only available on V8)
 		if (Error.captureStackTrace)
