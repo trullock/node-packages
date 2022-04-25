4d35b34d2bb0f31343d7cb0c0b085b254b230c14
Andrew Bullock <trullock@gmail.com>
2022-04-25 11:53:00 +0100

    fixed $ wrapper bug

--- dollar/src/index.js
+++ dollar/src/index.js
@@ -2,3 +2,6 @@ import * as querySelector from './querySelector.js'
 import * as events from './events.js'
 import * as domManipulation from './domManipulation.js'
 import * as utilities from './utilities.js'
+
+if(window)
+	window.$ = querySelector.$;
